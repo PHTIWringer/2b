e1195acf0c51c78d97ced4571ba5a5d04b6c5eaf
Ken Hileman <ken.hileman@outlook.com>
2024-04-02 20:01:03 -0400

    second commit assignment 2b

--- temp_convert.py
+++ temp_convert.py
@@ -3,7 +3,7 @@
 # Date: 04/02/2024
 # Description: Asks the user for a Celsius temperature and converts it to Fahrenheit
 
-print("Please enter a Celsius temperature")
+print("Please enter a Celsius temperature.")
 celsius_num = float(input())
 
 celsius_to_fahrenheit = ((9 / 5) * celsius_num + 32)
